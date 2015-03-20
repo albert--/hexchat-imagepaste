a29c23a09d40a2eb966e87004a547abf8574b099
albert-- <albert--@users.noreply.github.com>
2015-03-20 22:03:15 +0100

    vry  importante

--- imagepaste.py
+++ imagepaste.py
@@ -6,7 +6,7 @@ from imgurpython import ImgurClient
 
 __module_name__ = "hexchat-imagepaste"
 __module_author__ = "albert--"
-__module_version__ = "1.0"
+__module_version__ = "1.0.1"
 __module_description__ = "Paste images from your clipboard using imgur.com by pressing Alt +"
 
 client_id = "YOUR CLIENT ID"
@@ -31,8 +31,8 @@ def imagepaste(word, word_eol, userdata):
 	tempf.close()
 	remove(tempfn)
 	
-	cmd = "say " + up['link']
-	print("Deletelink: http://imgur.com/delete/" + up['deletehash'])
+	cmd = "say " + up['link'].replace("i.imgur.com", "b.bildgur.de")
+	print("Deletelink: http://imgur.com/delete/" + up['deletehash'].replace("imgur.com", "bildgur.de"))
 	
 	hexchat.command(cmd)
 	return hexchat.EAT_ALL
